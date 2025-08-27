b0eda8e9a11c41dc0b9dc415c45daf1d12e2b6ee
Vulfka <poblete.gnicolas@gmail.com>
2025-08-27 15:07:28 -0400

    Se añade el mensaje de error al ingresar números o carácteres especiales.

--- app.js
+++ app.js
@@ -2,12 +2,21 @@
 let listaAmigos = [];
 
 function agregarAmigo() {
+
     let nuevoAmigo = document.getElementById("amigo").value;
-    listaAmigos.push(nuevoAmigo);
-    document.getElementById("amigoAgregado").innerHTML += `<p>${nuevoAmigo}</p>`;
-    console.log(listaAmigos);
-    document.getElementById("amigo").value = "";
-    return listaAmigos;
+
+    if (/[^a-zA-ZáéíóúüñÁÉÍÓÚÜÑ\s]/g.test(nuevoAmigo)) {
+        document.querySelector(".error").classList.add("active");
+        document.getElementById("amigo").value = "";
+        return listaAmigos;
+    } else {
+        document.querySelector(".error").classList.remove("active");
+        listaAmigos.push(nuevoAmigo);
+        document.getElementById("amigoAgregado").innerHTML += `<p>${nuevoAmigo}</p>`;
+        console.log(listaAmigos);
+        document.getElementById("amigo").value = "";
+        return listaAmigos;
+    }
  
     
 
